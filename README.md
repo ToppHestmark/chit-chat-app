1fb82d6bac81433cbc36a32115a62d36be6478ba
Suphachai Hestmark <top_hestmark@yahoo.no>
2021-01-29 21:37:05 +0100

    initialize

---
+++ README.md
@@ -0,0 +1,9 @@
+# Chit-Chat
+
+Chat web app
+
+## Dependencies
+
+- [Ant design icons](https://github.com/ant-design/ant-design-icons)
+- [Axios](https://www.npmjs.com/package/axios)
+- [React-chat-engine](https://www.npmjs.com/package/react-chat-engine)
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from "react-chat-engine";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="05a43b28-f91f-4345-99c5-ca2c702ed0b4"
+      userName="TheMaster"
+      userSecret="honning"
+    />
+  );
+};
+
+export default App;
